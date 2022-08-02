30a9e5e611534537dc3cf3bed0fbd30c8cb384f8
zaryabdev <zaryabdev404@gmail.com>
2022-08-02 17:32:44 +0500

    routes working

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,26 +1,104 @@
 import React, { useState, useEffect } from 'react';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import Setup from './imprimer/Setup';
+import Grid from './imprimer/GridSample';
 import './App.css';
 import './styles/skeleton.css';
 import 'antd/dist/antd.css';
-import MainLayout from './imprimer/MainLayout';
+
+// import MainLayout from './imprimer/MainLayout';
+const { Content, Footer, Sider } = Layout;
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Bill', '0', <FileOutlined />),
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [
+    getItem('Team 1', '6'),
+    getItem('Team 2', '8'),
+  ]),
+];
 
 export default function App() {
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
     // <MainLayout />
     <Router>
-      <MainLayout />
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/meseros" element={<Meseros />} />
-      </Routes>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo" />
+          {/* <Menu
+            theme="light"
+            defaultSelectedKeys={['0']}
+            mode="inline"
+            items={items}
+          /> */}
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <FileOutlined />
+              <span>Setup</span>
+              <Link to="/setup" />
+            </Menu.Item>
+            <Menu.Item key="2">
+              <FileOutlined />
+              <span>Meseros</span>
+              <Link to="/grid" />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content style={{ margin: '0 16px' }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Routes>
+                <Route exact path="/" element={<Setup />} />
+                <Route path="/setup" element={<Setup />} />
+                <Route path="/grid" element={<Grid />} />
+              </Routes>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Imprimer ©2022 Created by Z labs
+          </Footer>
+        </Layout>
+      </Layout>
     </Router>
   );
-}
-function Dashboard(params: type) {
-  return <center>Dashboard</center>;
-}
-function Meseros(params: type) {
-  return <center>Meseros</center>;
 }
