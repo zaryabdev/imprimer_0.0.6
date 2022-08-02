c07bc9ad243cc2c78e451a06c2b5aef35fc9e218
zaryabdev <zaryabdev404@gmail.com>
2022-08-02 16:53:57 +0500

    adding routes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,18 +3,24 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 import './styles/skeleton.css';
-// import Setup2 from './Setup';
-import Grid from './GridSample';
-const Sample = () => {
-  return <div>Hi</div>;
-};
+import 'antd/dist/antd.css';
+import MainLayout from './imprimer/MainLayout';
 
 export default function App() {
   return (
+    // <MainLayout />
     <Router>
+      <MainLayout />
       <Routes>
-        <Route path="/" element={<Grid />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/meseros" element={<Meseros />} />
       </Routes>
     </Router>
   );
+}
+function Dashboard(params: type) {
+  return <center>Dashboard</center>;
+}
+function Meseros(params: type) {
+  return <center>Meseros</center>;
 }
